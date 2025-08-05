fda00b680374abebe47eb4307a2d3432ec2ba696
santoshkp098 <santosh.kumar@tejimandi.com>
2025-08-05 13:21:58 +0530

    added cron for reminder scheduling

--- controllers/reminderController.js
+++ controllers/reminderController.js
@@ -1,5 +1,6 @@
 const Reminder = require('../models/Reminder');
 const helperFunctions = require ('../utils/helperFunctions');
+const moment = require('moment-timezone');
 
 const saveReminder = async ({command, ack, say}) => {
   try {
@@ -89,4 +90,42 @@ const deleteReminder = async ({command, ack, say}) => {
   }
 }
 
-module.exports = {saveReminder, getReminders, updateReminders, deleteReminder};
+const startReminderScheduler = async (app) => {
+      const nowIST = moment().tz('Asia/Kolkata');
+      // Calculate start & end of the day (in minutes)
+      const startOfDayIST = moment.tz(nowIST.format('YYYY-MM-DD 00:00'), 'Asia/Kolkata').valueOf() / 1000;
+      const endOfDayIST = moment.tz(nowIST.format('YYYY-MM-DD 23:59'), 'Asia/Kolkata').valueOf() / 1000;
+
+      // Current time in epoch minutes
+      const currentEpochMinutes = Math.floor(
+        moment().tz("Asia/Kolkata").seconds(0).milliseconds(0).valueOf() / 1000
+      );
+
+      // Fetch reminders for today's IST date
+      const reminders = await Reminder.find({
+        status: 'pending',
+        triggerTime: { $gte: startOfDayIST, $lte: endOfDayIST }
+      }).lean();
+
+      for (const reminder of reminders) {
+        try {
+          // Send reminder only if it's exactly the current IST minute
+          if (reminder.triggerTime === currentEpochMinutes) {
+
+            await app.client.chat.postMessage({
+              channel: reminder.channelId,
+              text: `🔔 Hey <@${reminder.userId}>, ${reminder.reminderText}`,
+            });
+
+            await Reminder.updateOne(
+              { _id: reminder._id },
+              { $set: { status: 'sent' } }
+            );
+          }
+        } catch (innerErr) {
+          console.error(`Failed to send reminder ${reminder._id}:`, innerErr);
+        }
+      }
+}
+
+module.exports = {saveReminder, getReminders, updateReminders, deleteReminder, startReminderScheduler};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const { App } = require('@slack/bolt');
 const reminderController = require('./controllers/reminderController');
 const connectDB = require('./utils/db');
+const cron = require('node-cron');
+
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
@@ -17,5 +19,9 @@ const app = new App({
   app.command('/remindme-update', reminderController.updateReminders);
   app.command('/remindme-delete', reminderController.deleteReminder);
 
+  cron.schedule('* * * * *', async () => {
+    reminderController.startReminderScheduler(app);
+  });
+
   console.log('ReMindful app is running!');
-});
+})();
